f2ee8c36aaacd90eb1088e7fb24ce1e773acf583
BeeBercik <robertkania@spoko.pl>
2024-03-20 22:33:23 +0100

    Add Game in Bones
    I've spent a lot of time at this but my satisfaction was high when i end this.

--- Main.java
+++ Main.java
@@ -1,164 +1,49 @@
-package MyTasks.Ants;
+package MyTasks.graWkosci;
 
-import java.util.ArrayList;
-
-
-class Ant implements Runnable {
-    private Board board;
-    private int x;
-    private int y;
-
-    public Ant(Board board, int x, int y) {
-        this.board = board;
-        this.x = x;
-        this.y = y;
-    }
-
-    private void move() {
-        synchronized(this) {
-            int oldX = this.x;
-            int oldY = this.y;
-            int jumpToX = (int) (Math.random() * 3) + this.x - 1;
-            int jumpToY = (int) (Math.random() * 3) + this.y - 1;
-            this.x = jumpToX;
-            this.y = jumpToY;
-
-
-            System.out.println("Ant's name: " + Thread.currentThread().getName());
-            if((this.x < 0 || this.x > board.getGameSize() - 1) || (this.y < 0 || this.y > board.getGameSize() - 1)) {
-                this.deathAndBorn(oldX, oldY);
-                return;
-            }
-
-            if(board.getCell(this.x, this.y).equals(".")) {
-                System.out.println("Ant at (" + oldX + ", " + oldY + ") moves to (" + this.x + ", " + this.y + ").");
-                board.setCell(oldX, oldY, ".");
-                board.setCell(this.x, this.y, "8");
-            } else if(board.getCell(jumpToX, jumpToY).equals("8")) {
-                System.out.println("Ant at (" + this.x + ", " + this.y + ") stays at her position.");
-            }
-
-            board.printBoard();
-            System.out.println();
-            try {
-                long time = (long) (Math.random() * 3000) + 2000;
-                Thread.sleep(time);
-            } catch(InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    private void deathAndBorn(int oldX, int oldY) {
-        synchronized(this) {
-            while((this.x < 0 || this.x > board.getGameSize() - 1) || (this.y < 0 || this.y > board.getGameSize() - 1)) {
-                int newX = (int) (Math.random() * 3) + this.x - 1;
-                int newY = (int) (Math.random() * 3) + this.y - 1;
-                this.x = newX;
-                this.y = newY;
-            }
-            System.out.println("Ant at (" + oldX + ", " + oldY + ") dies, new ant at (" + this.x + ", " + this.y + ").");
-
-            board.setCell(oldX, oldY, ".");
-            board.setCell(this.x, this.y, "8");
-            board.printBoard();
-            try {
-                long time = (long) (Math.random() * 3000) + 2000;
-                Thread.sleep(time);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    @Override
-    public void run() {
-        while(true) {
-            this.move();
-        }
-    }
-}
-
-class Board {
-    private int gameSize;
-    private Cell[][] board;
-
-    public Board(int gameSize) {
-        this.gameSize = gameSize;
-        this.board = new Cell[gameSize][gameSize];
-    }
-
-    private class Cell {
-        String value;
-
-        public Cell(String value) {
-            this.value = value;
-        }
-
-        public String getValue() {
-            return this.value;
-        }
-    }
-
-    public void setCell(int x, int y, String value) {
-        Cell cell = new Cell(value);
-        this.board[x][y] = cell;
-    }
-
-    public String getCell(int x, int y) {
-        return this.board[x][y].getValue();
-    }
-
-    public void fillBoard() {
-        for (int i = 0; i < this.gameSize; i++) {
-            for (int j = 0; j < this.gameSize; j++) {
-                this.setCell(i, j, ".");
-            }
-        }
-    }
-
-    public void printBoard() {
-        for (int i = 0; i < this.gameSize; i++) {
-            for (int j = 0; j < this.gameSize; j++) {
-                System.out.print(this.getCell(i, j));
-            }
-            System.out.println();
-        }
-    }
-
-    public int getGameSize() {
-        return this.gameSize;
-    }
-}
+import java.util.Random;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        int gameSize = 10;
-        int startAntCount = 5;
+    public static void main(String[] args) {
+        byte max = 30;
+        byte firstPlayer = 0;
+        byte secondPlayer = 0;
+        Random random = new Random();
+        byte whoThrows = (byte) (random.nextInt(2) + 1);
+        byte quantity = 2;
 
-        Board board = new Board(gameSize);
-        board.fillBoard();
-
-        ArrayList<Thread> ants = new ArrayList<>();
-        int cond = 0;
-        while(cond < startAntCount) {
-            int x = (int) (Math.random() * 10);
-            int y = (int) (Math.random() * 10);
-
-            if(board.getCell(x, y).equals("8")) continue;
-            board.setCell(x, y, "8");
-
-            Thread ant = new Thread(new Ant(board, x, y), "Ant" + cond);
-            ants.add(ant);
-            cond++;
+        byte round = 1;
+        System.out.println("--Zaczyna gracz nr " + whoThrows + "--\n");
+        while(firstPlayer < max && secondPlayer < max) {
+            if(whoThrows % 2 == 0) {
+                byte playerResultInRound = 0;
+                for (int i = 0; i < quantity; i++) {
+                    byte thrown = (byte) (random.nextInt(6) + 1);
+                    playerResultInRound += thrown;
+                    System.out.println("Gracz 2 rzuca za " + thrown);
+                }
+                System.out.println("Wynik po " + round + " rundzie: " + playerResultInRound);
+                secondPlayer += playerResultInRound;
+                System.out.println("Calkowity wynik 2 gracza: " + secondPlayer + "\n");
+            } else {
+                byte playerResultInRound = 0;
+                for (int i = 0; i < quantity; i++) {
+                    byte thrown = (byte) (random.nextInt(6) + 1);
+                    playerResultInRound += thrown;
+                    System.out.println("Gracz 1 rzuca za " + thrown);
+                }
+                System.out.println("Wynik po " + round + " rundzie: " + playerResultInRound);
+                firstPlayer += playerResultInRound;
+                System.out.println("Calkowity wynik 1 gracza: " + firstPlayer + "\n");
+            }
+            whoThrows++;
+            round++;
         }
 
-        System.out.println("-STARTED BOAR-");
-        board.printBoard();
-        for (int i = 0; i < startAntCount; i++) {
-            ants.get(i).start(); // procek na podstawie czasu dostepu przydziela watki
-            Thread.sleep(1000);
-        }
-
+        System.out.println("--WYNIKI--");
+        System.out.println("Pierwszy gracz: " + firstPlayer);
+        System.out.println("Drugi gracz: " + secondPlayer);
+        System.out.println(
+                firstPlayer > secondPlayer ? "Wygrywa gracz nr 1, gratulacje!" : "Wygrywa gracz nr 2, gratulacje!"
+        );
     }
 }
